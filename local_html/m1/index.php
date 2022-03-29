1ab4ddda33f94b72364f2f68d09a3a7b612fe400
rsiena6 <rshall@rams.colostate.edu>
2022-03-28 22:54:25 -0600

    links

--- local_html/m1/index.php
+++ local_html/m1/index.php
@@ -1,3 +1,9 @@
-<a href = ../images/ramlog.png></a>
+<a href = ../images/ramlogo.png></a>
 
 <h1> Cam's Interior Design Services </h1>
+
+<br>
+<br>
+
+<a href="about.php">About Us</a>
+<a href="color.php"> Generate Color Coordinates </a>
