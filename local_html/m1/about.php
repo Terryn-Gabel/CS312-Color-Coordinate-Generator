3a0b6b0d8957804d3f6cb22e5d43d56ba999d33a
rachel hall <97854531+rsiena6@users.noreply.github.com>
2022-03-29 18:09:34 -0600

    Update about.php

--- local_html/m1/about.php
+++ local_html/m1/about.php
@@ -1,1 +1,6 @@
+<h1> About Us </h1>
+
+<p> Rachel Hall's biography: </p>
+<br>
+<p>  My name is Rachel Hall. I am a senior at Colorado State University. I have a cute dog and work for a Cybersecurity company! </p>
 
