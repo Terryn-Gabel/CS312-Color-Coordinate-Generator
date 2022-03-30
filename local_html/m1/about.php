af13d140982b4d4700e8f87cfa8bbcc91526a0e2
Rachel Hall <rshall@rams.colostate.edu>
2022-03-29 18:27:55 -0600

    image

--- local_html/m1/about.php
+++ local_html/m1/about.php
@@ -9,5 +9,6 @@
   <p> Rachel Hall </p>
   <br>
   <p> My name is Rachel Hall. I am a senior at Colorado State University. I have a cute dog and work for a Cybersecurity company! </p>
+  <a href="../images/rsiena6.png"></a>
 </body>
 </html>
