962f546aef91f487bb9155c09e95e738e396932c
turnercy <102266952+turnercy@users.noreply.github.com>
2022-03-30 13:23:30 -0600

    Update template.php

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -1,7 +1,7 @@
 <h1>Cam's Interior Design</h1>
 <nav>
-            <a href="https://www.cs.colostate.edu:4444/~hillyard/m1/index/mistone1/home#">Homepage</a> ||
-            <a href="https://www.cs.colostate.edu:4444/~hillyard/m1/index/mistone1/about">About</a> ||
-            <a href="">Color Coordinate Generation</a>
+            <a href="home">Homepage</a> ||
+            <a href="about">About</a> ||
+            <a href="color">Color Coordinate Generation</a>
 </nav>
 <?php echo $content; ?>
