6465d9893964358f5ca05a73a1c0fdf4a37b278c
AshHillyard <98000990+AshHillyard@users.noreply.github.com>
2022-03-29 20:21:53 -0600

    Update template.php

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -2,6 +2,6 @@
 <nav>
             <a href="https://www.cs.colostate.edu:4444/~hillyard/m1/index/mistone1/home#">Homepage</a> ||
             <a href="https://www.cs.colostate.edu:4444/~hillyard/m1/index/mistone1/about">About</a> ||
-            <a href="https://csurams.com/sports/track-and-field/schedule">Color Coordinate Generation</a>
+            //<a href="">Color Coordinate Generation</a>
 </nav>
 <?php echo $content; ?>
