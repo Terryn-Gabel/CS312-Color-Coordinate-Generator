c98c4f41d8e45f470c141742fc305f9b04e1461d
AshHillyard <98000990+AshHillyard@users.noreply.github.com>
2022-03-29 20:21:28 -0600

    Create template.php

---
+++ fuel/app/views/template.php
@@ -0,0 +1,7 @@
+<h1>Cam's Interior Design</h1>
+<nav>
+            <a href="https://www.cs.colostate.edu:4444/~hillyard/m1/index/mistone1/home#">Homepage</a> ||
+            <a href="https://www.cs.colostate.edu:4444/~hillyard/m1/index/mistone1/about">About</a> ||
+            <a href="https://csurams.com/sports/track-and-field/schedule">Color Coordinate Generation</a>
+</nav>
+<?php echo $content; ?>
