752e38e09c84c0695d03f0eb1dcb5bd03b02be51
AshHillyard <98000990+AshHillyard@users.noreply.github.com>
2022-03-29 20:13:23 -0600

    Create mistone1.php

---
+++ fuel/app/classes/controllers/mistone1.php
@@ -0,0 +1,27 @@
+<?php
+class Controller_Mistone1 extends Controller_Template
+{
+
+  public function action_home()
+  {
+    $data = array();
+    $this->template->title = 'Cams Interior Design';
+    $this->template->content = View::forge('pages/homepg', $data);
+  }
+  
+  public function action_about()
+  {
+    $data = array();
+    $this->template->title = 'Cams Interior Design About';
+    $this->template->content = View::forge('pages/Aboutpg', $data);
+  }
+  
+  public function action_color()
+  {
+    $data = array();
+    $this->template->title = 'Golddex';
+    $this->template->content = View::forge('pages/homepg', $data);
+  }
+}
+?>
+  
