fc9934d1d4118e40e5bc19173ea70d14ee9799a9
AshHillyard <98000990+AshHillyard@users.noreply.github.com>
2022-03-29 20:11:46 -0600

    Create Aboutpg.php

---
+++ fuel/app/views/pages/Aboutpg.php
@@ -0,0 +1,27 @@
+<?php
+$output = <<<OUTPUT
+<!DOCTYPE HTML>
+<html>
+
+<body>
+<h4> Rachel Hall </h4>
+  </br>
+    <p> My name is Rachel Hall. I am a senior at Colorado State University. I have a cute dog and work for a Cybersecurity company! </p>
+      <img src="https://raw.githubusercontent.com/Terryn-Gabel/CS312-Color-Coordinate-Generator/main/local_html/m1/assets/rsiena6.png?token=GHSAT0AAAAAABQW7BQZTM5ONRX26LOCKZVYYSDXBOA"></img>
+  
+<h4> Ashlyn Hillyard </h4>
+  </br>
+    <p>Hi my name is Ashlyn! I am a Junior enrolled in CS312, Modern Web Applictions, at Colorado State University. My ultimate goal is to create software that solves practical problems and makes people's lives easier. I am very passionate about software development and am extremely excited to be on a team with other individuals who share that same passion. In my free time I love running and am on the Cross Country and Track & Field team at CSU.</p>
+      <img src="https://raw.githubusercontent.com/Terryn-Gabel/CS312-Color-Coordinate-Generator/main/local_html/m1/assets/Ashlynphoto.jpg?token=GHSAT0AAAAAABQW7BQYD2ZRYZHCAJSGJQMGYSDXIEA"></img>
+<h4> Terryn Gabel </h4> 
+  </br>
+    <p>  My name is Terryn Gabel. I am a senior at Colorado State University, studying Computer Science, and a Software Engineer intern. In my spare time I enjoy playing video games and spoiling my cat. </p>
+      <img src="https://raw.githubusercontent.com/Terryn-Gabel/CS312-Color-Coordinate-Generator/main/local_html/m1/assets/Terrynphoto.png?token=GHSAT0AAAAAABQW7BQZIHDGILXRPXVQI34OYSDXAZA"></img>
+<h4> Cyrus Turner </h4>
+  </br>
+  <p> Hi my name is Cyrus, and I am the chief marketing officer at Cam’s Interior Design Services. I am also a Colorado State University student who is currently enrolled in CS 312 and is about to graduate. Outside of my professional life, I am passionate about playing and producing music. </p>
+<img src="https://raw.githubusercontent.com/Terryn-Gabel/CS312-Color-Coordinate-Generator/main/local_html/m1/assets/Cyrusphoto.png?token=GHSAT0AAAAAABQW7BQZ6JXRBFESA5N2RWAQYSDXAGA"</img>
+</body>
+</html>
+OUTPUT;
+echo $output;
