65ed258f2054a8b54e96593d5845981a83dd894e
Terryn <tg3213@gmail.com>
2022-03-30 21:33:37 -0600

    add fuel form to color.php

--- fuel/app/views/pages/color.php
+++ fuel/app/views/pages/color.php
@@ -1,33 +1,32 @@
-<!DOCTYPE HTML>
-<html lang = "en">
-  <title>Our Color Coordinate Page</title>
-  <head>
-  <script>
-      function validateInput() {
-         var rowcols = document.myForm.rowscol.value;
-         var color = document.myForm.colors.value;
-         
-         if (rowcols < 1 || rowcols > 26) {
-            alert("Please enter valid number of rows/columns, must be a number between 0 and 27!");
-            document.myForm.rowscol.focus();
-            return false;
-         }
-         if (color < 1 || color > 10) {
-            alert("Please enter valid number of colors, must be a number between 0 and 11!");
-            document.myForm.colors.focus();
-            return false;
-         }
-         return( true );
-      }
-  </script>
-  </head>
-  <body>
-    <form action = "color" name="myForm" onsubmit="return validateInput()">
-      <label for="rowscol">Number of Rows/Columns:</label>
-      <input type="number" min="1" max="26" name="rowscol" id="rowscol" required>
-      <label for="colors">Number of Colors:</label>
-      <input type="number" min="1" max="10" name="colors" id="colors" required>
-      <input type="submit" value="Submit">
-    </form>  
-  </body>
-</html>  
+
+<script>
+  <?php echo Asset::js('color.js'); ?>
+</script>
+
+<body>
+  <!-- <form action = "color" name="myForm" onsubmit="return validateInput()">
+    <label for="rowscol">Number of Rows/Columns:</label>
+    <input type="number" name="rowscol" id="rowscol" required>
+    <label for="colors">Number of Colors:</label>
+    <input type="number" name="colors" id="colors" required>
+    <input type="submit" value="Submit">
+  </form>   -->
+  <?php
+    echo Form::open(array(
+        "action" => "index/mistone1",
+        "method" => "get",
+        "id" => "fuel_form"
+    ));
+ 
+    echo Form::label('Number of Rows and Columns:&nbsp;', 'rowcol');
+    echo Form::input('rowcol', '', array('style' => 'border: 2px;'));
+    echo Form::label('<br>Number of Colors:&nbsp;', 'numcolors');
+    echo Form::input('numcolors', '', array('style' => 'border: 2px;'));
+
+    echo Form::submit('submitbtn', 'Submit', array('rowcol', 'numcolors'));
+
+    echo Form::close();
+?>
+</body>
+
+
