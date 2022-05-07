52b3828cea8af01d4ec991ce31a308bc7b6483c6
rachel hall <97854531+rsiena6@users.noreply.github.com>
2022-05-06 21:42:24 -0600

    better format

--- fuel/app/views/pages/homepg.php
+++ fuel/app/views/pages/homepg.php
@@ -1,20 +1,18 @@
 <body>
 
-
         <header>
-        
-            <h2>   Welcome!   </h2>
-            <div class="floatclear"><br></div>
-            <img class=studio></img>
+                <h2>   Welcome!   </h2>
+                <div class="floatclear"><br></div>
+                <img class=studio></img>
         </header>
+        
         <div class="floatclear"><br></div>
+       
         <main>
             <div>
                 
-
-            <h3> We are a locally owned and operated paint studio located in Old Town Fort Collins.</h3>
-                
-            <h3> Come into our physical location to paint your heart out or visit our <a href="./color">virtual paint tool</a> to get started. </h3>
+                <h3> We are a locally owned and operated paint studio located in Old Town Fort Collins.</h3>
+                <h3> Come into our physical location to paint your heart out or visit our <a href="./color">virtual paint tool</a> to get started. </h3>
                 
             </div> 
 
